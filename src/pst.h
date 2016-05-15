04f2c0e03d14260e4685bb89a4efd1d5c3881b1e
Srdja Matovic <s.matovic@app26.de>
2016-05-15 12:58:03 +0200

    added pst-h, piece square tables and static eval values

--- src/pst.h
+++ src/pst.h
@@ -34,7 +34,7 @@
 /* const Score EvalPieceValues[9] = {0, 100, 300, 0, 300, 500, 900}; */
 const Score EvalPieceValues[9] = {0, 100, 400, 0, 400, 600, 1200};
 
-/* square control bonus,black view */
+/* square control bonus, black view */
 /* flop square for white-index: sq^56*/
 const Score EvalControl[64] =
 {
