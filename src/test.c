98f3bb3452b50a21f27addc0798fe3c14cc2a412
Srdja Matovic <s.matovic@app26.de>
2016-05-16 10:49:40 +0200

    added test

---
+++ src/test.c
@@ -0,0 +1,29 @@
+/*
+  Name:         Zeta Dva
+  Description:  Amateur level chess engine
+  Author:       Srdja Matovic <s.matovic@app26.de>
+  Created at:   2011-01-15
+  Updated at:   2016-07-13
+  License:      GPL >= v2
+
+  Copyright (C) 2011-2016 Srdja Matovic
+
+  Zeta Dva is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  Zeta Dva is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+*/
+
+#include <stdio.h>      /* for print and scan */
+#include <stdlib.h>     /* for malloc free */
+#include <string.h>     /* for string compare */ 
+
+#include "timer.h"      /* for time measurement */
+#include "types.h"      /* custom types, board defs, data structures, macros */
+
+
---
+++ src/test.h
@@ -0,0 +1,28 @@
+/*
+  Name:         Zeta Dva
+  Description:  Amateur level chess engine
+  Author:       Srdja Matovic <s.matovic@app26.de>
+  Created at:   2011-01-15
+  Updated at:   2016-07-13
+  License:      GPL >= v2
+
+  Copyright (C) 2011-2016 Srdja Matovic
+
+  Zeta Dva is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  Zeta Dva is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+*/
+
+#include "types.h"      /* custom types, board defs, data structures, macros */
+
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+
+#endif /* TEST_H_INCLUDED */
+
