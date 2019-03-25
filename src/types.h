987c28017d5ab21e7eac649cd85f5acf0e10a18a
Srdja Matovic <s.matovic@app26.de>
2019-03-25 13:06:46 +0100

    version bump

--- src/types.h
+++ src/types.h
@@ -58,7 +58,7 @@ typedef u64             Move;
 typedef u64             File;
 typedef u64             Rank;
 
-#define VERSION         "0309"
+#define VERSION         "0310"
 /* quad bitboard array index definition */
 #define QBBBLACK  0     /* pieces white */
 #define QBBP1     1     /* piece type first bit */
